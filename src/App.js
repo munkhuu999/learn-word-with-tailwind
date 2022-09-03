b057d86e7c192ae55d7e68c5dbf171e797203ce4
munkhuu999 <bmbr589@gmail.com>
2022-09-04 00:00:24 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className=' border-red-400 bg-teal-400 mx-36'>
+        <h1 className=' text-lg font-bold text-red-700 mt-11'>сайн уу мөөгий мину уууу урагшаа  чаднаа чи</h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
